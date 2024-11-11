444801aca6e1ae05b373496eecb7991bcb5fcdb5
Sami Alyasin <alyasin@marshall.edu>
2024-11-11 17:15:54 -0500

    Pushing some updates to the ARIMA modeling section

--- Pages/Project.py
+++ Pages/Project.py
@@ -428,7 +428,6 @@ with st.container(border=True):
     st.markdown('''
     ### ARIMA
     ''')
-
     st.code('''
     import pandas as pd
     import numpy as np
@@ -436,28 +435,58 @@ with st.container(border=True):
     import statsmodels.api as sm
     import matplotlib.pyplot as plt
     from sklearn.metrics import mean_absolute_error, root_mean_squared_error, r2_score
+    from sklearn.model_selection import train_test_split, TimeSeriesSplit, cross_val_score
 
-    # Load the data
+    # get historical data for APPL for the last 20 years
     data_ARIMA = pd.read_csv('stock_data.csv', index_col='Date', parse_dates=True)
 
     # Filtering data for a single stock to implement ARIMA model
     stock_ticker = 'AAPL'
     stock_data = data_ARIMA[data_ARIMA['Ticker'] == stock_ticker]['Adj Close']
 
+    # Split the data into training and test sets
+    train_data, test_data = train_test_split(stock_data, test_size=0.2, shuffle=False)
+
     # Differencing to make time series stationary
-    differenced_data = stock_data.diff().dropna()
+    differenced_data = train_data.diff().dropna()
+
+    # Augmented Dickey-Fuller test to check for stationarity
+    from statsmodels.tsa.stattools import adfuller
+    result1 = adfuller(train_data)
+    print(f'Training data ADF Statistic: {result1[0]}')
+    print(f'Training data p-value: {result1[1]}')
+    print("Original data is stationary" if result1[1] <= 0.05 else "Original data is not stationary, differencing required")
+
+    result2 = adfuller(differenced_data)
+    print(f'Differenced data ADF Statistic: {result2[0]}')
+    print(f'Differenced data p-value: {result2[1]}')
+    print("Differenced data is stationary" if result2[1] <= 0.05 else "Differenced data is not stationary")
 
     # Finding the order (p, d, q) using ACF and PACF plots
     fig, ax = plt.subplots(2, 1, figsize=(20, 8))
-    sm.graphics.tsa.plot_acf(differenced_data, lags=20, ax=ax[0])
-    sm.graphics.tsa.plot_pacf(differenced_data, lags=20, ax=ax[1])
+    sm.graphics.tsa.plot_acf(differenced_data, ax=ax[0])
+    sm.graphics.tsa.plot_pacf(differenced_data, ax=ax[1])
     plt.show()
     ''')
     
-    # st.image('https://raw.githubusercontent.com/Sami-Alyasin/Crystal-Stockball/refs/heads/main/data/acf_pacf.png')
+    st.text('''
+            Training data ADF Statistic: -2.3613780288367243
+            Training data p-value: 0.1529351125960524
+            Original data is not stationary, differencing required
+
+            Differenced data ADF Statistic: -31.084016077072704
+            Differenced data p-value: 0.0
+            Differenced data is stationary
+            ''')
+    
+    st.image('https://raw.githubusercontent.com/Sami-Alyasin/Crystal-Stockball/refs/heads/main/data/acf_pacf.png')
     
     st.markdown('''
-    ### ACF and PACF Interpretation
+    ### AFD, ACF, & PACF Interpretation
+
+    - **ADF (Augmented Dickey-Fuller) test:** 
+        - The ADF test we ran suggests that our original data is not stationary (p-value: 0.1529), meaning it likely has trends or other non-stationary components that ARIMA cannot handle well directly, and that the differenced data is stationary (p-value: 0.0).
+        - The ADF Statistic value of -31.084 in the differenced data is a very large negative number, which indicates that the series is highly stationary.
     - **ACF Plot (Autocorrelation Function):**
         - The ACF plot shows the correlation of the time series with its past values (lags).
         - The significant spike at **lag 0** is expected since a time series is always perfectly correlated with itself.
@@ -467,16 +496,79 @@ with st.container(border=True):
         - The PACF plot shows the partial correlation of the time series with its own lagged values, after removing the effect of intermediate lags.
         - The **cut-off point** in the PACF plot helps identify the order of the Autoregressive (`p`) component.
         - In our plot, we see a significant spike at **lag 0**, followed by most subsequent lags falling within the confidence bands. This suggests that **`p` should also be around 0 or 1**.
+                ''')
+   
+    st.code('''
+            # Fit ARIMA model
+            p, d, q = 1, 1, 1
+            model = sm.tsa.ARIMA(stock_data, order=(p, d, q))
+            model_fit = model.fit()
 
-    ### Determining the Order (p, d, q):
+            # Model Summary
+            print(model_fit.summary())
 
-    - **`d` (differencing):**
-        - We already performed **first differencing** to make the time series stationary, which means `d = 1`.
-    - **`p` (Autoregressive term):**
-        - Since there is a significant spike at lag 0 in the PACF plot and no significant spikes after, it suggests **`p = 0 or 1`**.
-    - **`q` (Moving Average term):**
-        - The ACF plot has no significant spikes beyond lag 0, indicating a **`q` value of 0 or 1**.    
-    ''')
+            # Predicting the next day's price
+            forecast = model_fit.forecast(steps=1)
+            print(f'Predicted Adjusted Close Price for next trading day: {forecast.iloc[0]}')
+
+            # Plotting actual vs predicted
+            plt.figure(figsize=(20, 8))
+            plt.plot(stock_data[-100:], label='Actual Price')
+            plt.plot(model_fit.fittedvalues[-100:], label='Fitted Values', color='red')
+            plt.xlabel('Date')
+            plt.ylabel('Adjusted Close Price')
+            plt.title(f'Actual vs Fitted Values for {stock_ticker} Stock Price')
+            plt.legend()
+            plt.show()
+
+            # Evaluating Model Performance
+            y_true = stock_data[1:len(model_fit.fittedvalues) + 1]  # True values (excluding first row because of differencing)
+            y_pred = model_fit.fittedvalues[:len(y_true)]
+            mae = mean_absolute_error(y_true, y_pred)
+            rmse = root_mean_squared_error(y_true, y_pred)
+            mse = np.square(rmse)
+            r2 = r2_score(y_true, y_pred)   
+
+            print(f'MAE: {mae}')
+            print(f'MSE: {mse}')
+            print(f'RMSE: {rmse}')
+            print(f'R2 Score: {r2}')
+            ''') 
+    
+    st.text('''
+                                           SARIMAX Results                                
+        ==============================================================================
+        Dep. Variable:              Adj Close   No. Observations:                  936
+        Model:                 ARIMA(1, 2, 1)   Log Likelihood               -2223.847
+        Date:                Mon, 11 Nov 2024   AIC                           4453.694
+        Time:                        17:10:36   BIC                           4468.213
+        Sample:                             0   HQIC                          4459.231
+                                        - 936                                         
+        Covariance Type:                  opg                                         
+        ==============================================================================
+                        coef    std err          z      P>|z|      [0.025      0.975]
+        ------------------------------------------------------------------------------
+        ar.L1         -0.0169      0.029     -0.586      0.558      -0.074       0.040
+        ma.L1         -0.9999      0.130     -7.668      0.000      -1.256      -0.744
+        sigma2         6.7992      0.907      7.494      0.000       5.021       8.578
+        ===================================================================================
+        Ljung-Box (L1) (Q):                   0.01   Jarque-Bera (JB):               109.34
+        Prob(Q):                              0.94   Prob(JB):                         0.00
+        Heteroskedasticity (H):               1.19   Skew:                            -0.05
+        Prob(H) (two-sided):                  0.12   Kurtosis:                         4.67
+        ===================================================================================
+        
+        
+                ''')
+    
+    st.image('https://raw.githubusercontent.com/Sami-Alyasin/Crystal-Stockball/refs/heads/main/data/ARIMA.png')
+    
+    st.text('''
+            MAE: 13.31685522450983
+            MSE: 298.6497810176591
+            RMSE: 17.281486655310044
+            R2 Score: 0.39032436821929584
+                        ''')
 
     st.code('''
             # Append ARIMA & SARIMA results to the evaluation DataFrame
